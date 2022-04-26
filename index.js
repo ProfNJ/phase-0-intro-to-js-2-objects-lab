fc3be8dbcd019839b0eb620c51125616e62c50fe
Rory Grant <rorygran5@gmail.com>
2022-04-26 09:39:10 -0400

    commit tree e8c30b2dfcf9aaa02eaef61841bf26138063ec31

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your solution in this file!
+const employee = {};
+employee.name = "Name";
+employee.streetAddress = "5th Ave";
+
+
+function updateEmployeeWithKeyAndValue(obj, key, value){
+    let nObj = {...obj};
+    nObj[key] = value;
+    return nObj;
+
+}
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value){
+    obj[key] = value;
+    return obj;
+}
+
+function deleteFromEmployeeByKey(obj, key){
+    let nObj = {...obj};
+    delete nObj[key];
+    return nObj;
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj, key){
+    delete obj[key];
+    return obj;
+
+}
